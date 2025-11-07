eaf0a1793d6d1bf5c5e1723026a871aed1a9a760
Iparigl784 <iparigl784@g.educaand.es>
2025-11-07 16:20:53 +0100

    Firebase actualizado

--- full90/src/App.jsx
+++ full90/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import SignInForm from "./components/SignInForm_useState";
+import SignUpFormEstadosAgrupados from "./components/SignUpForm_useRefEstadosAgrupados";
+import SignUpForm from "./components/SignUpForm_useRef";
+import Contador from "./components/Contador";
+import BigLayout from "./pages/BigLayout";
+import Error from "./pages/Error";
+import AdminPanel from "./pages/AdminPanel";
+import Home from "./pages/Home";
+import "./styles/globales.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<BigLayout />}>
+        <Route index element={<Home />} />
+        <Route path="contadorUseEffect" element={<Contador />} />
+        <Route path="loginUseState" element={<SignInForm />} />
+        <Route
+          path="registroUseRefEstadosAgrupados"
+          element={<SignUpFormEstadosAgrupados />}
+        />
+        <Route
+          path="registroUseRefEstadosSinAgrupar"
+          element={<SignUpForm />}
+        />
+
+        <Route path="admin" element={<AdminPanel />} />
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- full90/src/main.jsx
+++ full90/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { UserContextProvider } from "./contexts/ContextUser";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
